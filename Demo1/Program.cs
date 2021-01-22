1ae30ddee3d0b27d161da025553294c2de2ed156
Helena Copppieters <hcoppieters@hotmail.com>
2021-01-22 12:00:06 +0100

    Add project files.

---
+++ Demo1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Demo1
+{
+    class Hond :IEquatable<Hond> //Named Type
+    { 
+        public string Naam { get; set; }
+        public int Leeftijd { get; set; }
+
+        public bool Equals([AllowNull] Hond other)
+        {
+            return Naam == other.Naam && Leeftijd == other.Leeftijd;
+        }
+    }
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            //Aanmaken van een anonymous type:
+            var hond1 = new { Naam = "Bobby", Leeftijd = 2 };
+            var hond2 = new { Naam = "Bobby", Leeftijd = 2 };
+            Console.WriteLine(hond1.Naam + " Leeftijd = " + hond1.Leeftijd);
+            Console.WriteLine("Hond1 is gelijk aan hond2? " + hond1.Equals(hond2));
+
+            Hond hond3 = new Hond { Naam = "Bobby", Leeftijd = 2 };
+            Hond hond4 = new Hond { Naam = "Bobby", Leeftijd = 2 };
+
+            Console.WriteLine(hond3.Naam + " Leeftijd = " + hond3.Leeftijd);
+            Console.WriteLine("Hond3 is gelijk aan hond4? " + hond3.Equals(hond4));
+        }
+    }
+}
---
+++ Demo2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+namespace Demo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var puntenReeks = new[]
+            {
+                new { X=0,Y=1},
+                new { X=1,Y=1},
+                new { X=2, Y=-1}
+            };
+            int aantalMetXGroterDan0 =  puntenReeks.Where(p => p.X >= 1).Count();
+            int somVanYwaarden = puntenReeks.Sum(p => p.Y);
+            int somVanYwaarden2 =
+                    (from p in puntenReeks
+                     select p).Sum(p => p.Y);
+        }
+    }
+}
